f3d06838701d77fb652ba9220f984d9ceffc0bdc
Jyoti0129 <jyotipal1323@gmail.com>
2021-01-04 21:39:07 +0530

    done

---
+++ app.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector("#btn-translate");
+ var txtInput = document.querySelector("#txt-input");
+ var outputDiv =document.querySelector("#output");
+
+  
+ var serverURL ="https://api.funtranslations.com/translate/yoda.json"
+
+ function getTranslationURL(input){
+     return serverURL + "?" + "text=" +  input
+ }
+
+ function errorHandler(error){
+    console.log("error occured", error);
+     alert("something wrong with server! try again after some time");
+
+ }
+
+ function clickEventHandler(){
+   
+
+  var inputText = txtInput.value;
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+     outputDiv.innerHTML =  json.contents.translated;
+   })
+   .catch(errorHandler)
+
+} 
+ 
+ btnTranslate.addEventListener("click", clickEventHandler)
+
